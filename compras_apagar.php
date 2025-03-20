c82e70e2ef170a8dc96b9064d3b4b86c63f2dc8c
luiszzk780 <luiscarlospizzatto@gmail.com>
2025-03-20 11:37:40 -0300

    aula5

---
+++ compras_apagar.php
@@ -0,0 +1,11 @@
+<?php
+
+require('inc/banco.php');
+$id = $_GET['id']?? null;
+
+if (id){
+    $query = $pdo->prepare('DELETE FROM compras where id =:id');
+}
+    $query->execute([
+        'id' =>$id
+    ]);
